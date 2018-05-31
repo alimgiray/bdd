cf283cc94cb4156cbcb0bd8c592aadae4f571869
alimgiray <alimgiray@gmail.com>
2018-05-31 16:29:01 +0300

    Refactor

--- src/main/java/com.alimgiray.bdd/core/message/SoapMessage.java
+++ src/main/java/com.alimgiray.bdd/core/message/SoapMessage.java
@@ -1,5 +1,6 @@
 package com.alimgiray.bdd.core.message;
 
+import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.xembly.Directives;
@@ -9,6 +10,7 @@ import org.xml.sax.InputSource;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -26,30 +28,37 @@ public class SoapMessage extends XmlMessage {
     private static final String SOAP_NS_URI = "http://schemas.xmlsoap.org/soap/envelope/";
 
     public SoapMessage() {
-        this.soapenv = new Namespace("soapenv", SOAP_NS_URI);
-        this.header = new ArrayList<>();
-        this.body = new ArrayList<>();
-        this.root = new ComplexXmlField("root");
+        setup();
     }
-
     public SoapMessage(String xml) throws IOException, JDOMException {
-        this();
+
+        setup();
+
         SAXBuilder saxBuilder = new SAXBuilder();
-        org.jdom2.Document document = saxBuilder.build(new InputSource(new StringReader(xml)));
-        super.createFromString(document.getRootElement(), this.root);
+        Document document = saxBuilder.build(new InputSource(new StringReader(xml)));
+
+        createFromString(document.getRootElement(), this.root);
 
         //Çevirirken header body bakmadan çeviriyor, sonradan çomar gibi headerı bodyi set ediyorum burada indexiyle.
         //FIXME: burayı düşüneceğim
         ComplexXmlField soapHeader = (ComplexXmlField) ((ComplexXmlField) this.root.getXmlFields().get(0)).getXmlFields().get(0);
         ComplexXmlField soapBody = (ComplexXmlField) ((ComplexXmlField) this.root.getXmlFields().get(0)).getXmlFields().get(1);
+
         for (XmlField headerElement : soapHeader.getXmlFields()) {
             addFieldToHeader(headerElement);
         }
+
         for (XmlField headerElement : soapBody.getXmlFields()) {
             addFieldToBody(headerElement);
         }
     }
 
+    private void setup() {
+        this.soapenv = new Namespace("soapenv", SOAP_NS_URI);
+        this.header = new ArrayList<>();
+        this.body = new ArrayList<>();
+        this.root = new ComplexXmlField("root");
+    }
 
     public List<XmlField> getHeader() {
         return header;
@@ -81,29 +90,36 @@ public class SoapMessage extends XmlMessage {
 
     @Override
     public String toString() {
-        ComplexXmlField envelope = new ComplexXmlField("Envelope");
-        envelope.setNamespace(this.soapenv);
-        ComplexXmlField header = new ComplexXmlField("Header");
-        header.setNamespace(this.soapenv);
-        ComplexXmlField body = new ComplexXmlField("Body");
-        body.setNamespace(this.soapenv);
-        for (XmlField headerElement : this.header) {
-            header.addField(headerElement);
-        }
-        for (XmlField bodyElement : this.body) {
-            body.addField(bodyElement);
-        }
-        envelope.addField(header);
-        envelope.addField(body);
+
+        ComplexXmlField envelope = getField("Envelope");
+        ComplexXmlField header = getField("Header");
+        ComplexXmlField body = getField("Body");
+
+        populateField(header, this.header);
+        populateField(body, this.body);
+        populateField(envelope, Arrays.asList(header, body));
+
         Directives directives = new Directives();
-        super.buildDirectives(envelope, directives);
-        String soapEnvelope = "";
+        buildDirectives(envelope, directives);
+
         try {
-            soapEnvelope = new Xembler(directives).xml();
+            return new Xembler(directives).xml();
         } catch (Exception e) {
             e.printStackTrace();
+            return "";
         }
-        return soapEnvelope;
-
     }
+
+    private void populateField(ComplexXmlField field, List<XmlField> childFields) {
+        for (XmlField element : childFields) {
+            field.addField(element);
+        }
+    }
+
+    private ComplexXmlField getField(String fieldName) {
+        ComplexXmlField complexXmlField = new ComplexXmlField(fieldName);
+        complexXmlField.setNamespace(this.soapenv);
+        return complexXmlField;
+    }
+
 }
---
+++ src/main/java/com.alimgiray.bdd/core/subject/SoapClient.java
@@ -0,0 +1,19 @@
+package com.alimgiray.bdd.core.subject;
+
+/**
+ * @author aaytar
+ * @since 31.05.2018 16:11
+ */
+public class SoapClient {
+
+    private String endpointUrl;
+
+    public SoapClient(String endpointUrl) {
+        this.endpointUrl = endpointUrl;
+    }
+
+    public void send() {
+
+    }
+
+}
